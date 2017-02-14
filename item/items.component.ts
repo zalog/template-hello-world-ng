0df7f4b72c1c397127294fbb2179629177ac707d
Nikolay Tsonev <ntsonev@mcsofntsonev.bedford.progress.com>
2017-02-14 13:44:25 +0200

    remove Label tap event and add ListView itemTap

--- item/items.component.ts
+++ item/items.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { RouterExtensions } from "nativescript-angular"
+import { ItemEventData } from "ui/list-view";
 
 import { Item } from "./item";
 import { ItemService } from "./item.service";
@@ -11,9 +13,14 @@ import { ItemService } from "./item.service";
 export class ItemsComponent implements OnInit {
     items: Item[];
 
-    constructor(private itemService: ItemService) { }
+    constructor(private itemService: ItemService, private routerExtensions:RouterExtensions) { }
 
     ngOnInit(): void {
         this.items = this.itemService.getItems();
+    }
+
+    onItemTap(args:ItemEventData){
+        let itemID = this.items[args.index].id;
+        this.routerExtensions.navigate(['/item', itemID] )
     }
 }
